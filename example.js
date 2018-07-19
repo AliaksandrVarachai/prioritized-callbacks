746ebe6595260c8f018ddab273369208d3fe4272
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-07-19 18:55:47 +0300

    Config

---
+++ example.js
@@ -0,0 +1,35 @@
+import getPrioritizedMiddleware from './index';
+
+function f1(next) {
+  setTimeout(() => {
+    console.log('f1 is called');
+    next()
+  }, 1000);
+}
+function f2(next) {
+  console.log('f2 is called');
+  next();
+}
+function f3(next) {
+  console.log('f3 is called');
+  next();
+}
+
+const prioritizedMiddleware1 = getPrioritizedMiddleware('myEventName');
+const prioritizedMiddleware2 = getPrioritizedMiddleware('myEventName');
+
+prioritizedMiddleware1.usePriority(f3, 30);
+prioritizedMiddleware2.usePriority(f2, 20);
+prioritizedMiddleware2.usePriority(f1, 10);
+
+/*
+  output:
+  f1 is called
+  f2 is called
+  f3 is called
+  prioritized go called from module #1
+ */
+prioritizedMiddleware1.goPriority(function() {
+  console.log('prioritized go called from module #1');
+});
+
