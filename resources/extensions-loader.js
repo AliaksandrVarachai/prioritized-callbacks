78452cddfd1889ad9ef1ba91074d4b55cbd9afc8
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-07-30 22:37:58 +0300

    Add examples to extensions-loader

--- resources/extensions-loader.js
+++ resources/extensions-loader.js
@@ -4,6 +4,19 @@
  * 2. separates the developing process from affecting of other users
  * The idea is the loading of prioritized-event-bus.js synchronously before any tool scripts to provide the reliable
  * eventListener for the tool loaded event. Other extensions might be loaded asynchronously or in their own way.
+ *
+ * Example 1.
+ * If it needs to load feedback-tool script from the localhost (https://localhost:9092) use next query param
+ * ?test_ft_path=localhost%3A9092
+ *
+ * Example 2.
+ * If it needs to load feedback-tool script remotedly from the custom path (feedback-tool/<some-path>) use next query param
+ * ?test_ft_path=<some-path>
+ *
+ * Example 3.
+ * If it needs to load all scripts guided-tour.js, feedback-tool.js and prioritized-event-bus from the local servers
+ * use next query params for that:
+ *    ?test_gt_path=localhost%3A9090&test_ft_path=localhost%3A9092&test_peb_path=localhost%3A9093
  */
 (function() {
   var scripts = {
